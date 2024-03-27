b5f3f09e99a3ad8b81db796b8e0473811ffa855b
hoyhingithubacc2 <148970212+hoyhingithubacc2@users.noreply.github.com>
2024-03-27 19:23:49 +0800

    Update rustdesk.sh

--- rustdesk.sh
+++ rustdesk.sh
@@ -2,10 +2,22 @@
 #setup.sh NGROK_AUTH_TOKEN
 
 
-#install eve
+#install everyting
+sudo apt update && upgrade
+sudo apt install ufw
+ufw allow 21115:21119/tcp
+ufw allow 8000/tcp
+ufw allow 21116/udp
+sudo ufw enable
+wget https://raw.githubusercontent.com/techahold/rustdeskinstall/master/install.sh
+chmod +x install.sh
+./install.sh
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+./hbbs
+./hbbr
+cat ./id_ed25519.pub
 
 #configure ngrok and start it
 ngrok config add-authtoken 2XFrlRXy7iQBYRoayFVOmT64Kdj_7LXWFgZAgmvpSj6mogedb 
